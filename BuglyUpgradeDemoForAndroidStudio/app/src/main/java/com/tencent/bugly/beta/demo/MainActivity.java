0804bbf1b9fcb03c59ad8f996db2070890bd6aea
vellhe <vellhe@tencent.com>
2016-04-07 15:29:38 +0800

    * 分开AndroidStudio和Eclipse两个demo

---
+++ BuglyUpgradeDemoForAndroidStudio/app/src/main/java/com/tencent/bugly/beta/demo/MainActivity.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (C) 2016 The RDT of Wireless R&D in MIG. All right reversed.
+ * <p/>
+ * Created by vellhe on 16/4/7.
+ */
+package com.tencent.bugly.beta.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.tencent.bugly.beta.Beta;
+import com.tencent.bugly.beta.UpgradeInfo;
+
+public class MainActivity extends Activity {
+
+    Button checkUpgradeBtn;
+    Button refreshBtn;
+    TextView upgradeInfoTv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkUpgradeBtn = $(R.id.check_upgrade);
+        refreshBtn = $(R.id.refresh_info);
+        upgradeInfoTv = $(R.id.upgrade_info);
+
+        checkUpgradeBtn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                /***** 检查更新 *****/
+                Beta.checkUpgrade();
+            }
+        });
+
+        refreshBtn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                loadUpgradeInfo();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadUpgradeInfo();
+    }
+
+    private void loadUpgradeInfo() {
+        if (upgradeInfoTv == null)
+            return;
+
+        /***** 获取升级信息 *****/
+        UpgradeInfo upgradeInfo = Beta.getUpgradeInfo();
+
+        if (upgradeInfo == null) {
+            upgradeInfoTv.setText("无升级信息");
+            return;
+        }
+
+        StringBuilder info = new StringBuilder();
+        info.append("id: ").append(upgradeInfo.id).append("\n");
+        info.append("标题: ").append(upgradeInfo.title).append("\n");
+        info.append("升级说明: ").append(upgradeInfo.newFeature).append("\n");
+        info.append("versionCode: ").append(upgradeInfo.versionCode).append("\n");
+        info.append("versionName: ").append(upgradeInfo.versionName).append("\n");
+        info.append("发布时间: ").append(upgradeInfo.publishTime).append("\n");
+        info.append("安装包Md5: ").append(upgradeInfo.apkMd5).append("\n");
+        info.append("安装包下载地址: ").append(upgradeInfo.apkUrl).append("\n");
+        info.append("安装包大小: ").append(upgradeInfo.fileSize).append("\n");
+        info.append("弹窗间隔（ms）: ").append(upgradeInfo.popInterval).append("\n");
+        info.append("弹窗次数: ").append(upgradeInfo.popTimes).append("\n");
+        info.append("发布类型（0:测试 1:正式）: ").append(upgradeInfo.publishType).append("\n");
+        info.append("弹窗类型（1:建议 2:强制 3:手工）: ").append(upgradeInfo.upgradeType);
+
+        upgradeInfoTv.setText(info);
+    }
+
+    /**
+     * 代替findViewById
+     * 
+     * @param resId
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public <T> T $(int resId) {
+        return (T) findViewById(resId);
+    }
+}
